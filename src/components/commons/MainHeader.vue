efe996680325f2797a88ff549508b0f7bd901843
Silver <aethermauri@gmail.com>
2024-08-13 22:17:11 -0600

    changes

--- src/components/commons/MainHeader.vue
+++ src/components/commons/MainHeader.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        <h3>Aqui va el Main Header</h3>
+  <header class="bg-gray-800 text-white p-4">
+    <div class="container mx-auto flex justify-between items-start">
+      <h1 class="text-xl font-bold">{{ title }}</h1>
+      <nav>
+        <ul class="flex space-x-4 justify-between items-start">
+          <li><a href="#" class="hover:underline">Home</a></li>
+          <li><a href="#" class="hover:underline">About</a></li>
+          <li><a href="#" class="hover:underline">Contact</a></li>
+        </ul>
+      </nav>
     </div>
+  </header>
 </template>
 
 <script>
-    export default {
-    name: 'MainHeader'
-   }
+export default {
+  name: 'MainHeader',
+  data() {
+    return {
+      title: 'Mi Proyecto'
+    }
+  }
+}
 </script>
 
 <style scoped>
-
+/* Si es necesario, puedes agregar estilos adicionales aquí. */
 </style>
-
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import MainHeader from './components/commons/MainHeader.vue'
 
 const app = createApp(App)
 
