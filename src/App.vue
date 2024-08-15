31a8dbb1dfd0e0787e246331ed3b1cd6277640bb
Eduardo Lutrillo <lalovaferoz117@gmail.com>
2024-08-14 23:56:21 -0600

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
   <div>
     <MainHeader />
     <!-- Aquí puedes agregar otros componentes o contenido -->
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
 
 <script>
 import MainHeader from './components/commons/MainHeader.vue';
+
 
 export default {
   name: 'App',
@@ -18,7 +17,6 @@ export default {
   }
 };
 </script>
-
 <style>
 /* Tu CSS global o específico para App.vue */
 </style>
---
+++ src/components/commons/MainContent.vue
@@ -0,0 +1,18 @@
+<template>
+  
+    <div>
+        <h1>ddddddd</h1>
+    <div>
+</template>
+
+<script>
+    export default {
+  data() {
+    return {
+      title:
+    }
+  }
+}
+</script>
+
+<style scoped></style>
