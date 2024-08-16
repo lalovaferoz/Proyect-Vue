a2d4cddf69c6adacd0dfd033f37fe2f70ab0d491
Silver <aethermauri@gmail.com>
2024-08-15 20:09:16 -0600

    backgound aplicated

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <MainHeader />
-    <Footer />
-    <!-- Aquí puedes agregar otros componentes o contenido -->
-    <div
-      class="w-full h-full"
-      style="background-image: url('https://cdn.pixabay.com/photo/2017/08/24/13/04/abstract-2676727_1280.jpg')"></div>
+    <div class="flex-grow">
+      <!-- Contenedor para la imagen de fondo -->
+      <div 
+        class="bg-cover bg-center w-full min-h-[841px]" 
+        :style="{ backgroundImage: `url(${imageUrl})` }">
+        <!-- Aquí puedes agregar más contenido si es necesario -->
+      </div>
+    </div>
+    <MainFooter />
   </div>
 </template>
 
 <script>
-import MainHeader from './components/commons/MainHeader.vue'
+import MainHeader from './components/commons/MainHeader.vue';
+import MainFooter from './components/commons/MainFooter.vue';
+import imageUrl from '@/assets/img/bg2.jpg'; // Importa la imagen
 
 export default {
   name: 'App',
   components: {
-    MainHeader
+    MainHeader,
+    MainFooter,
+  },
+  data() {
+    return {
+      imageUrl
+    };
   }
-}
+};
 </script>
+
 <style>
 /* Tu CSS global o específico para App.vue */
 </style>
---
+++ src/components/commons/MainFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div class="bg-slate-800 h-16">
+      <p class="text-white">{{ title }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'MainFooter',
+  data() {
+    return {
+      title: 'MainFooter'
+    }
+  }
+}
+</script>
