14d4733f281c870b623021525dd4ccc7a984db86
Eduardo Lutrillo <lalovaferoz117@gmail.com>
2024-08-15 19:02:23 -0600

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
     <MainHeader />
+    <Footer />
     <!-- Aquí puedes agregar otros componentes o contenido -->
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div
+      class="w-full h-full"
+      style="background-image: url('https://cdn.pixabay.com/photo/2017/08/24/13/04/abstract-2676727_1280.jpg')"></div>
   </div>
 </template>
 
 <script>
-import MainHeader from './components/commons/MainHeader.vue';
-
+import MainHeader from './components/commons/MainHeader.vue'
 
 export default {
   name: 'App',
   components: {
     MainHeader
   }
-};
+}
 </script>
 <style>
 /* Tu CSS global o específico para App.vue */
---
+++ src/components/commons/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <div class="bg-slate-800"></div>
+
+    </footer>
+</template> 
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      title: 'Network'
+    }
+  }
+}
+
+</script>
