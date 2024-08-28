b117637b8c4f9c3054dda705a7780810fa0364cd
Silver <aethermauri@gmail.com>
2024-08-28 16:15:56 -0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <MainHeader />
-    
-      <!-- Contenedor con color sólido de fondo -->
-      <div class="bg-slate-500 w-full h-full min-h-[837px]">
-        <!-- Aquí puedes agregar más contenido si es necesario -->
-     
+
+    <!-- Contenedor con color sólido de fondo -->
+    <div class="bg-slate-500 w-full h-full min-h-[837px]">
+      <!-- Aquí puedes agregar más contenido si es necesario -->
     </div>
     <MainFooter />
   </div>
 </template>
 
 <script>
-import MainHeader from './components/commons/MainHeader.vue';
-import MainFooter from './components/commons/MainFooter.vue';
+import MainHeader from './components/commons/MainHeader.vue'
+import MainFooter from './components/commons/MainFooter.vue'
 
 export default {
   name: 'App',
   components: {
     MainHeader,
-    MainFooter,
-  },
-};
+    MainFooter
+  }
+}
 </script>
 
 <style>
 /* Tu CSS global o específico para App.vue */
 </style>
->
+
